f75b0e3e24af5cf876feced6941a37d01f2a532a
jgurda <johnnyjgurda@gmail.com>
2018-12-04 21:06:59 +0200

    Первый коммит

---
+++ test0/js.js
@@ -0,0 +1,28 @@
+
+// let test = {
+//     value: 'lol',
+//     value2: 50
+// };
+// console.log(test["value"]);
+// let art = ['1.jpg','2.jpg','3.jpg','4.jpg'];
+// console.log(art[0]);
+// alert(art[0]);
+// let answer = confirm("wtf?");
+// alert(answer);
+// let answer = prompt("wtf?","yeah!");
+// console.log(typeof (test.value));
+
+// let incr = 10,
+//     decr = 10;
+
+//     console.log(incr++);
+//     console.log(decr--);
+
+//     console.log(5%2);
+
+//     console.log("2" == 2);
+
+    var isChacked = false,
+        isClosed = false;
+
+        console.log(isChacked || !isClosed);
